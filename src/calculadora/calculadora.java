53a7aaef8a01c074134f2c0d666964943fb10ea8
thyago pacheco <thyagoluizrj3@gmail.com>
2022-03-02 22:43:26 -0300

    projeto commit

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,42 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("digiti o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("digiti o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+
+        System.out.println("somar; " + somar);
+        System.out.println("subtrair; " + subtrair);
+        System.out.println("multiplicar; " + multiplicar);
+        System.out.println("dividir; " + dividir);
+
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+    public static double dividir(double a, double b) {
+        return a / b;
+    }
+
+
+}
